f5edd6447f556cbb60c9d8421cb8b6e989c7eb90
fakturaserviceTWL <twl@fakturaservice.dk>
2024-05-09 20:00:04 +0200

    Adding Oxalis error codes

---
+++ src/OxalisErrorCode.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Fakturaservice\Edelivery;
+
+abstract class OxalisErrorCode
+{
+    const E_APR21014 = "E-APR21014";
+    const E_APR21016 = "E-APR21016";
+    const E_APR21019 = "E-APR21019";
+    const E_APR21020 = "E-APR21020";
+    const E_APR21021 = "E-APR21021";
+    const E_APR21022 = "E-APR21022";
+    const E_APR21023 = "E-APR21023";
+    const E_APR24123 = "E-APR24123";
+    const E_APR32001 = "E-APR32001";
+    const E_APR32002 = "E-APR32002";
+    const E_APR32003 = "E-APR32003";
+    const E_APS21014 = "E-APS21014";
+    const E_APS21016 = "E-APS21016";
+    const E_APS21019 = "E-APS21019";
+    const E_APS21020 = "E-APS21020";
+    const E_APS21021 = "E-APS21021";
+    const E_APS21022 = "E-APS21022";
+    const E_APS21023 = "E-APS21023";
+    const E_APS24001 = "E-APS24001";
+    const E_APS24002 = "E-APS24002";
+    const E_APS24003 = "E-APS24003";
+    const E_APS24004 = "E-APS24004";
+    const E_APS24005 = "E-APS24005";
+    const E_APS24006 = "E-APS24006";
+    const E_APS24007 = "E-APS24007";
+    const E_APS24999 = "E-APS24999";
+    const E_REF10001 = "E-REF10001";
+    const E_REF23001 = "E-REF23001";
+    const W_APR21015 = "W-APR21015";
+    const W_APR21017 = "W-APR21017";
+    const W_APS21015 = "W-APS21015";
+    const W_APS21017 = "W-APS21017";
+    const W_APS32301 = "W-APS32301";
+
+}
